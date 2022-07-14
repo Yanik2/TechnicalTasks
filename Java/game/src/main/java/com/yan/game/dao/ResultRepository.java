cd0ffb737df98301552500fe94a99149e07b4626
yan_zinchenko <yzinchenko@easytech.co>
2022-07-14 09:37:56 +0300

    remove redundant imports

--- Java/game/src/main/java/com/yan/game/dao/ResultRepository.java
+++ Java/game/src/main/java/com/yan/game/dao/ResultRepository.java
@@ -2,10 +2,6 @@ package com.yan.game.dao;
 
 import com.yan.game.entity.Result;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-
-import java.util.List;
 
 public interface ResultRepository extends JpaRepository<Result, Integer> {
-
 }
